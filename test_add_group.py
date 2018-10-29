27dfaebae3aa6f50f03f2d133e006ecfdafaa7e2
Emeraldany <emeraldany@gmail.com>
2018-10-29 23:43:55 +0200

    Удалены не использующиеся строки кода Из методов убраны параметры по умолчанию Добавлен тест "Добавление группы с пустями полями"

--- test_add_group.py
+++ test_add_group.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 import unittest
+
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -13,9 +11,18 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wb = self.wb
         self.open_home_page(wb)
-        self.login(wb)
+        self.login(wb, user="admin", password="secret")
         self.open_groups_page(wb)
-        self.create_group(wb)
+        self.create_group(wb, name="New test group", header="Test group header", footer="Test group footer")
+        self.return_to_groups_page(wb)
+        self.logout(wb)
+
+    def test_add_empty_group(self):
+        wb = self.wb
+        self.open_home_page(wb)
+        self.login(wb, user="admin", password="secret")
+        self.open_groups_page(wb)
+        self.create_group(wb, name="", header="", footer="")
         self.return_to_groups_page(wb)
         self.logout(wb)
 
@@ -26,49 +33,35 @@ class TestAddGroup(unittest.TestCase):
 
         wb.find_element_by_link_text("group page").click()
 
-    def create_group(self, wb):
+    def create_group(self, wb, name, header, footer):
         # start group creation
         wb.find_element_by_name("group_name").click()
         # populate group's form page
         wb.find_element_by_name("group_name").clear()
-        wb.find_element_by_name("group_name").send_keys("New test group")
+        wb.find_element_by_name("group_name").send_keys(name)
         wb.find_element_by_name("group_header").click()
         wb.find_element_by_name("group_header").clear()
-        wb.find_element_by_name("group_header").send_keys("Test group header")
+        wb.find_element_by_name("group_header").send_keys(header)
         wb.find_element_by_name("group_footer").click()
         wb.find_element_by_name("group_footer").clear()
-        wb.find_element_by_name("group_footer").send_keys("Test group footer")
+        wb.find_element_by_name("group_footer").send_keys(footer)
         # Submit group creation
         wb.find_element_by_name("submit").click()
 
     def open_groups_page(self, wb):
         wb.find_element_by_name("new").click()
 
-    def login(self, wb):
+    def login(self, wb, user, password):
         wb.find_element_by_name("user").clear()
-        wb.find_element_by_name("user").send_keys("admin")
+        wb.find_element_by_name("user").send_keys(user)
         wb.find_element_by_id("LoginForm").click()
         wb.find_element_by_name("pass").click()
         wb.find_element_by_name("pass").clear()
-        wb.find_element_by_name("pass").send_keys("secret")
+        wb.find_element_by_name("pass").send_keys(password)
         wb.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wb):
         wb.get("http://localhost/addressbook/group.php")
-
-    # def is_element_present(self, how, what):
-    #     try:
-    #         self.wb.find_element(by=how, value=what)
-    #     except NoSuchElementException as e:
-    #         return False
-    #     return True
-    #
-    # def is_alert_present(self):
-    #     try:
-    #         self.wb.switch_to_alert()
-    #     except NoAlertPresentException as e:
-    #         return False
-    #     return True
 
     def tearDown(self):
         self.wb.quit()
