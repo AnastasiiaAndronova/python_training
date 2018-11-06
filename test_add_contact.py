2a627ec5fe92037a23f7359bfc8be96d033ac260
Emeraldany <emeraldany@gmail.com>
2018-11-06 22:27:27 +0200

    1. Создан модуль application куда вынесены все вспомогательные методы + методы инициализация и закрытие сессии браузера 2. 1. Создан модуль application, куда вынесены все вспомогательные методы + методы инициализация и закрытие сессии браузера 3. В модули тестов добавлена функция инициализации фикстуры + вызовы фикстуры в код с тестами для групп и контактов, убраны тестовые классы 3. Существующие тесты полностью перестроены на pytest

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,127 +1,36 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from contact import Contact
-import unittest
+import pytest
+from application import Application
 
-class TestAddContact(unittest.TestCase):
-    def setUp(self):
-        self.wb = webdriver.Firefox()
-        self.wb.implicitly_wait(30)
-    
-    def test_add_contact(self):
-        self.open_home_page()
-        self.login(user="admin", password="secret")
-        self.open_add_group_page()
-        self.create_new_contact(Contact(firstname="test first name", middlename="test middle name", lastname="test last name",nickname="test nick name",
-                                    title="test title", company="Test company", address="Test adress", home="Test home", mobile="Test mobile", work="Test work", fax="Test fax",
-                                    email="Test email", email2="Test email 2", email3="Test email 3", homepage="Test homepage", address2="Test address2", phone2="Test phone 2",
-                                    notes="Test notes", birthday_day = "1", birthday_month = "October", birthday_year = "1990", anniversary_day = "2", anniversary_month = "May", anniversary_year = "2000"))
-        self.return_to_home_page()
-        self.logout()
+@ pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_empty_contact(self):
-        self.open_home_page()
-        self.login(user="admin", password="secret")
-        self.open_add_group_page()
-        self.create_new_contact(Contact(firstname="", middlename="", lastname="",nickname="",
-                                    title="", company="", address="", home="", mobile="", work="", fax="",
-                                    email="", email2="", email3="", homepage="", address2="", phone2="",
-                                    notes="", birthday_day = "", birthday_month="", birthday_year ="", anniversary_day="", anniversary_month="", anniversary_year=""))
-        self.return_to_home_page()
-        self.logout()
+def test_add_contact(app):
+    app.open_home_page()
+    app.login(user="admin", password="secret")
+    app.open_add_contact_page()
+    app.create_new_contact(Contact(firstname="test first name", middlename="test middle name", lastname="test last name",nickname="test nick name",
+                                title="test title", company="Test company", address="Test adress", home="Test home", mobile="Test mobile", work="Test work", fax="Test fax",
+                                email="Test email", email2="Test email 2", email3="Test email 3", homepage="Test homepage", address2="Test address2", phone2="Test phone 2",
+                                notes="Test notes", birthday_day = "1", birthday_month = "October", birthday_year = "1990", anniversary_day = "2", anniversary_month = "May", anniversary_year = "2000"))
+    app.return_to_home_page()
+    app.logout()
 
+def test_add_empty_contact(app):
+    app.open_home_page()
+    app.login(user="admin", password="secret")
+    app.open_add_contact_page()
+    app.create_new_contact(Contact(firstname="", middlename="", lastname="", nickname="",
+                                       title="", company="", address="", home="", mobile="", work="", fax="",
+                                       email="", email2="", email3="", homepage="", address2="", phone2="",
+                                       notes="", birthday_day = "", birthday_month="", birthday_year ="", anniversary_day="", anniversary_month="", anniversary_year=""))
+    app.return_to_home_page()
+    app.logout()
 
-    def return_to_home_page(self):
-        wb = self.wb
-        wb.find_element_by_link_text("home page").click()
-
-    def create_new_contact(self, Contact):
-        wb = self.wb
-        # populate "add contact" fields
-        # Хотелось сделать универсальный метод populate_form_fields но пока непонятно как лучше "правильно" хранить множество свойств для одних и тех же обьектов.
-        wb.find_element_by_name("firstname").clear()
-        wb.find_element_by_name("firstname").send_keys(Contact.firstname)
-        wb.find_element_by_name("middlename").clear()
-        wb.find_element_by_name("middlename").send_keys(Contact.middlename)
-        wb.find_element_by_name("lastname").clear()
-        wb.find_element_by_name("lastname").send_keys(Contact.lastname)
-        wb.find_element_by_name("nickname").clear()
-        wb.find_element_by_name("nickname").send_keys(Contact.nickname)
-        wb.find_element_by_name("title").clear()
-        wb.find_element_by_name("title").send_keys(Contact.title)
-        wb.find_element_by_name("company").clear()
-        wb.find_element_by_name("company").send_keys(Contact.company)
-        wb.find_element_by_name("address").clear()
-        wb.find_element_by_name("address").send_keys(Contact.address)
-        wb.find_element_by_name("home").clear()
-        wb.find_element_by_name("home").send_keys(Contact.address2)
-        wb.find_element_by_name("mobile").clear()
-        wb.find_element_by_name("mobile").send_keys(Contact.mobile)
-        wb.find_element_by_name("work").clear()
-        wb.find_element_by_name("work").send_keys(Contact.work)
-        wb.find_element_by_name("fax").clear()
-        wb.find_element_by_name("fax").send_keys(Contact.fax)
-        wb.find_element_by_name("email").clear()
-        wb.find_element_by_name("email").send_keys(Contact.email)
-        wb.find_element_by_name("email2").clear()
-        wb.find_element_by_name("email2").send_keys(Contact.email2)
-        wb.find_element_by_name("email3").clear()
-        wb.find_element_by_name("email3").send_keys(Contact.email3)
-        wb.find_element_by_name("homepage").clear()
-        wb.find_element_by_name("homepage").send_keys(Contact.homepage)
-        # Fill fields with date
-
-        # Обрабатываем случай, когда послали для выбора из списка пустое значение:
-        # это скорее всего не очень хорошее решение, но в конкретно нашем случае тесты работоспособны
-        if Contact.birthday_day is not "":
-            Select(wb.find_element_by_name("bday")).select_by_visible_text(Contact.birthday_day)
-        if Contact.birthday_month is not "":
-            Select(wb.find_element_by_name("bmonth")).select_by_visible_text(Contact.birthday_month)
-        wb.find_element_by_name("byear").clear()
-        wb.find_element_by_name("byear").send_keys(Contact.birthday_year)
-        if Contact.anniversary_day is not "":
-            Select(wb.find_element_by_name("aday")).select_by_visible_text(Contact.anniversary_day)
-        if Contact.anniversary_month is not "":
-            Select(wb.find_element_by_name("amonth")).select_by_visible_text(Contact.anniversary_month)
-        wb.find_element_by_name("ayear").clear()
-        wb.find_element_by_name("ayear").send_keys(Contact.anniversary_year)
-
-        wb.find_element_by_name("address2").clear()
-        wb.find_element_by_name("address2").send_keys(Contact.address2)
-        wb.find_element_by_name("phone2").clear()
-        wb.find_element_by_name("phone2").send_keys(Contact.phone2)
-        wb.find_element_by_name("notes").clear()
-        wb.find_element_by_name("notes").send_keys(Contact.notes)
-        # Submit contact creation
-        wb.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-    def open_add_group_page(self):
-        wb = self.wb
-        wb.find_element_by_link_text("add new").click()
-
-    def login(self, user, password):
-        wb = self.wb
-        wb.find_element_by_name("user").clear()
-        wb.find_element_by_name("user").send_keys(user)
-        wb.find_element_by_id("LoginForm").click()
-        wb.find_element_by_name("pass").click()
-        wb.find_element_by_name("pass").clear()
-        wb.find_element_by_name("pass").send_keys(password)
-        wb.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self):
-        wb = self.wb
-        wb.get("http://localhost/addressbook/")
-
-    def logout(self):
-        wb = self.wb
-        wb.find_element_by_link_text("Logout").click()
-
-    def tearDown(self):
-        self.wb.quit()
-
-if __name__ == "__main__":
-    unittest.main()
 
 
--- test_add_group.py
+++ test_add_group.py
@@ -1,74 +1,29 @@
-# -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
 from group import Group
+from application import Application
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wb = webdriver.Firefox()
-        self.wb.implicitly_wait(30)
+@ pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_group(self):
-        self.open_home_page()
-        self.login(user="admin", password="secret")
-        self.open_groups_page()
-        self.create_group(Group(name="New test group", header="Test group header", footer="Test group footer"))
-        self.return_to_groups_page()
-        self.logout()
+def test_add_group(app):
+    app.open_home_page()
+    app.login(user="admin", password="secret")
+    app.open_groups_page()
+    app.open_add_group_page()
+    app.create_group(Group(name="New test group", header="Test group header", footer="Test group footer"))
+    app.return_to_groups_page()
+    app.logout()
 
-    def test_add_empty_group(self):
-        self.open_home_page()
-        self.login(user="admin", password="secret")
-        self.open_groups_page()
-        self.create_group(Group(name="", header="", footer=""))
-        self.return_to_groups_page()
-        self.logout()
+def test_add_empty_group(app):
+    app.open_home_page()
+    app.login(user="admin", password="secret")
+    app.open_groups_page()
+    app.open_add_group_page()
+    app.create_group(Group(name="", header="", footer=""))
+    app.return_to_groups_page()
+    app.logout()
 
-    def logout(self):
-        wb = self.wb
-        wb.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self):
-        wb = self.wb
-        wb.find_element_by_link_text("group page").click()
-
-    def create_group(self,group):
-        wb = self.wb
-        # start group creation
-        wb.find_element_by_name("group_name").click()
-        # populate group's form page
-        wb.find_element_by_name("group_name").clear()
-        wb.find_element_by_name("group_name").send_keys(group.name)
-        wb.find_element_by_name("group_header").click()
-        wb.find_element_by_name("group_header").clear()
-        wb.find_element_by_name("group_header").send_keys(group.header)
-        wb.find_element_by_name("group_footer").click()
-        wb.find_element_by_name("group_footer").clear()
-        wb.find_element_by_name("group_footer").send_keys(group.footer)
-        # Submit group creation
-        wb.find_element_by_name("submit").click()
-
-    def open_groups_page(self):
-        wb = self.wb
-        wb.find_element_by_name("new").click()
-
-    def login(self, user, password):
-        wb = self.wb
-        wb.find_element_by_name("user").clear()
-        wb.find_element_by_name("user").send_keys(user)
-        wb.find_element_by_id("LoginForm").click()
-        wb.find_element_by_name("pass").click()
-        wb.find_element_by_name("pass").clear()
-        wb.find_element_by_name("pass").send_keys(password)
-        wb.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self):
-        wb = self.wb
-        wb.get("http://localhost/addressbook/group.php")
-
-    def tearDown(self):
-        self.wb.quit()
-
-if __name__ == "__main__":
-    unittest.main()
