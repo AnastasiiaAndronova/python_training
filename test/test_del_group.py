0b715e2bf013e95ce00795cda6be3bc3733c737b
Emeraldany <emeraldany@gmail.com>
2018-11-12 14:53:26 +0200

    добавлен тест для удаления первой группы и первого контакта добавлен тест для изменения контакта

--- test/test_del_group.py
+++
@@ -1,7 +0,0 @@
-
-def test_delete_first_group(app):
-    app.open_login_page()
-    app.session.login(user="admin", password="secret")
-    app.group.open_groups_page()
-    app.group.delete_first_group()
-    app.session.logout()
