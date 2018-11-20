6522741e64eb1109c1465680f2fba1cb2a1b2da8
Emeraldany <emeraldany@gmail.com>
2018-11-20 21:22:16 +0200

    Добавлен вспомогательный метод для проверки количества элементов в списках групп и получения их атрибутов Тесты для групп доработаны для работы с этим методом.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,11 +1,17 @@
 from model.group import Group
 
 def test_add_group(app):
+    current_groups = app.group.get_group_list()
     app.group.create(Group(name="New test group",
                            header="Test group header",
                            footer="Test group footer"))
+    new_groups = app.group.get_group_list()
+    assert len(current_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    current_groups = app.group.get_group_list()
     app.group.create(Group())
+    new_groups = app.group.get_group_list()
+    assert len(current_groups) + 1 == len(new_groups)
 
--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -5,6 +5,10 @@ def test_delete_first_group(app):
     app.group.open_groups_page()
     if app.group.count() == 0:
         app.group.create(Group())
+    current_groups = app.group.get_group_list()
     app.group.delete_first()
+    new_groups = app.group.get_group_list()
+    assert len(current_groups) -1 == len(new_groups)
+
 
 # пока что только первая группа
