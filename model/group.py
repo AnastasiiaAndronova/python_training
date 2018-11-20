5a8bdb634853582bdd32e49e51b72f91246f7813
Emeraldany <emeraldany@gmail.com>
2018-11-20 21:41:56 +0200

    Переопределены стандартные методы сравнения и репрезентации (пока для групп) Доработан метод удаления группы (проверка что новый список равен старому, но без именно первого элемента)

--- model/group.py
+++ model/group.py
@@ -8,38 +8,12 @@ class Group:
         self.id = id
 
 
-class Contact:
+# метод определяет как будут выглядеть обьекты при выводе на консоль
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
 
-    def __init__(self, firstname, middlename, lastname, nickname, title,
-                 company, address, home, mobile, work, fax,
-                 email, email2, email3, homepage, address2, phone2, notes,
-                 birthday_day, birthday_month, birthday_year, anniversary_day, anniversary_month, anniversary_year):
-
-        # form fields
-        self.firstname = firstname
-        self.middlename = middlename
-        self.lastname = lastname
-        self.nickname = nickname
-        self.title = title
-        self.company = company
-        self.address = address
-        self.home = home
-        self.mobile = mobile
-        self.work = work
-        self.fax = fax
-        self.email = email
-        self.email2 = email2
-        self.email3 = email3
-        self.homepage = homepage
-        self.address2 = address2
-        self.phone2 = phone2
-        self.notes = notes
-        # For dates:
-        self.birthday_day = birthday_day
-        self.birthday_month = birthday_month
-        self.birthday_year = birthday_year
-        self.anniversary_day = anniversary_day
-        self.anniversary_month = anniversary_month
-        self.anniversary_year = anniversary_year
+# метод сравнения
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
 
 
--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -5,10 +5,12 @@ def test_delete_first_group(app):
     app.group.open_groups_page()
     if app.group.count() == 0:
         app.group.create(Group())
-    current_groups = app.group.get_group_list()
+    old_groups = app.group.get_group_list()
     app.group.delete_first()
     new_groups = app.group.get_group_list()
-    assert len(current_groups) -1 == len(new_groups)
+    assert len(old_groups) -1 == len(new_groups)
+    old_groups[0:1]=[]
+    assert old_groups == new_groups
 
 
 # пока что только первая группа
