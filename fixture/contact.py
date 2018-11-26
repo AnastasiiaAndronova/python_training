0228337bcebbba4bbadb2dea1ddcb5c555674393
Emeraldany <emeraldany@gmail.com>
2018-11-26 22:21:45 +0200

    Переименовала аргументы методов, чтобы не совпадали с именем класса

--- fixture/contact.py
+++ fixture/contact.py
@@ -27,55 +27,55 @@ class ContactHelper:
         if not (wd.current_url.endswith("addressbook/edit.php") and len(wd.find_elements_by_name("submit")) > 0 and len(wd.find_elements_by_name("firstname"))):
             wd.find_element_by_link_text("add new").click()
 
-    def create(self, Contact):
+    def create(self, contact):
         wd = self.app.wd
         self.open_add_contact_page()
-        self.fill_the_form(Contact)
+        self.fill_the_form(contact)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         self.contact_cache = None
 
-    def fill_the_form(self, Contact):
-        self.app.actions.change_field_value("firstname", Contact.firstname)
-        self.app.actions.change_field_value("middlename", Contact.middlename)
-        self.app.actions.change_field_value("lastname", Contact.lastname)
-        self.app.actions.change_field_value("nickname", Contact.nickname)
+    def fill_the_form(self, contact):
+        self.app.actions.change_field_value("firstname", contact.firstname)
+        self.app.actions.change_field_value("middlename", contact.middlename)
+        self.app.actions.change_field_value("lastname", contact.lastname)
+        self.app.actions.change_field_value("nickname", contact.nickname)
         # if Contact.photo is not None:
         #     wd.find_element_by_name("photo").send_keys(Contact.photo)
-        self.app.actions.change_field_value("title", Contact.title)
-        self.app.actions.change_field_value("company", Contact.company)
-        self.app.actions.change_field_value("address", Contact.address)
-        self.app.actions.change_field_value("home", Contact.home)
-        self.app.actions.change_field_value("address2", Contact.address2)
-        self.app.actions.change_field_value("mobile", Contact.mobile)
-        self.app.actions.change_field_value("work", Contact.work)
-        self.app.actions.change_field_value("fax", Contact.fax)
-        self.app.actions.change_field_value("email", Contact.email)
-        self.app.actions.change_field_value("email2", Contact.email2)
-        self.app.actions.change_field_value("email3", Contact.email3)
-        self.app.actions.change_field_value("homepage", Contact.homepage)
-        self.app.actions.select_in_dropdown("bday", Contact.birthday_day)
-        self.app.actions.change_field_value("byear", Contact.birthday_year)
-        self.app.actions.select_in_dropdown("bmonth", Contact.birthday_month)
-        self.app.actions.select_in_dropdown("aday", Contact.anniversary_day)
-        self.app.actions.select_in_dropdown("amonth", Contact.anniversary_month)
-        self.app.actions.change_field_value("ayear", Contact.anniversary_year)
-        self.app.actions.change_field_value("address2", Contact.address2)
-        self.app.actions.change_field_value("phone2", Contact.phone2)
-        self.app.actions.change_field_value("notes", Contact.notes)
+        self.app.actions.change_field_value("title", contact.title)
+        self.app.actions.change_field_value("company", contact.company)
+        self.app.actions.change_field_value("address", contact.address)
+        self.app.actions.change_field_value("home", contact.home)
+        self.app.actions.change_field_value("address2", contact.address2)
+        self.app.actions.change_field_value("mobile", contact.mobile)
+        self.app.actions.change_field_value("work", contact.work)
+        self.app.actions.change_field_value("fax", contact.fax)
+        self.app.actions.change_field_value("email", contact.email)
+        self.app.actions.change_field_value("email2", contact.email2)
+        self.app.actions.change_field_value("email3", contact.email3)
+        self.app.actions.change_field_value("homepage", contact.homepage)
+        self.app.actions.select_in_dropdown("bday", contact.birthday_day)
+        self.app.actions.change_field_value("byear", contact.birthday_year)
+        self.app.actions.select_in_dropdown("bmonth", contact.birthday_month)
+        self.app.actions.select_in_dropdown("aday", contact.anniversary_day)
+        self.app.actions.select_in_dropdown("amonth", contact.anniversary_month)
+        self.app.actions.change_field_value("ayear", contact.anniversary_year)
+        self.app.actions.change_field_value("address2", contact.address2)
+        self.app.actions.change_field_value("phone2", contact.phone2)
+        self.app.actions.change_field_value("notes", contact.notes)
 
-    def modify_first(self, Contact):
+    def modify_first(self, contact):
         wd = self.app.wd
         self.app.contact.open_homepage()
         self.open_for_edit_by_index(0)
-        self.fill_the_form(Contact)
+        self.fill_the_form(contact)
         wd.find_element_by_name("update").click()
         self.contact_cache = None
 
-    def modify_by_index(self, Contact, index):
+    def modify_by_index(self, contact, index):
         wd = self.app.wd
         self.app.contact.open_homepage()
         self.open_for_edit_by_index(index)
-        self.fill_the_form(Contact)
+        self.fill_the_form(contact)
         wd.find_element_by_name("update").click()
         self.contact_cache = None
 
